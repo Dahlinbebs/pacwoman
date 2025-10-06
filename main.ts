fccf2017c0181b38f6e222fd50004ff97d24c1d7
Dahlinbebs <Louise.oestergaard.dahlin@hotmail.dk>
2025-10-06 22:10:08 +0200

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,209 +1,134 @@
-function pacManMoveLeft () {
-    if (controller.left.isPressed()) {
-        animation.runImageAnimation(
-        mySprite,
-        [img`
-            . . . . 5 5 5 5 5 5 5 5 . . . . 
-            . . . 5 5 5 5 5 5 5 5 5 5 . . . 
-            . . 5 5 5 5 5 5 f f 5 5 5 5 . . 
-            . 5 5 5 5 5 5 5 f f 5 5 5 5 . . 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            . . . . . . 5 5 5 5 5 5 5 5 5 5 
-            . . . . . . . . 5 5 5 5 5 5 5 5 
-            . . . . . . . . 5 5 5 5 5 5 5 5 
-            . . . . . . 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            . . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-            . . . 5 5 5 5 5 5 5 5 5 5 5 . . 
-            . . . . . 5 5 5 5 5 5 5 5 5 . . 
-            . . . . . . 5 5 5 5 5 . . . . . 
-            `,img`
-            . . . . 5 5 5 5 5 5 5 5 . . . . 
-            . . . 5 5 5 5 5 5 5 5 5 5 . . . 
-            . . 5 5 5 5 5 5 f f 5 5 5 5 . . 
-            . 5 5 5 5 5 5 5 f f 5 5 5 5 . . 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            . . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-            . . . 5 5 5 5 5 5 5 5 5 5 5 . . 
-            . . . . . 5 5 5 5 5 5 5 5 5 . . 
-            . . . . . . 5 5 5 5 5 . . . . . 
-            `],
-        100,
-        true
-        )
-    }
-}
-function pacManMoveDown () {
-    if (controller.down.isPressed()) {
-        animation.runImageAnimation(
-        mySprite,
-        [img`
-            . . . . 5 5 5 5 5 5 5 . . . . . 
-            . . . 5 5 5 5 5 5 5 5 5 . . . . 
-            . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-            . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-            . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 f f 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 f f 5 5 
-            5 5 5 5 5 5 5 . . 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 . . 5 5 5 5 5 5 5 
-            . 5 5 5 5 5 . . . . 5 5 5 5 5 5 
-            . . 5 5 5 5 . . . . 5 5 5 5 5 5 
-            . . 5 5 5 5 . . . . 5 5 5 5 5 . 
-            . . . 5 5 5 . . . . 5 5 5 5 . . 
-            . . . . 5 5 . . . . 5 5 5 . . . 
-            . . . . . 5 . . . . 5 5 . . . . 
-            `,img`
-            . . . . 5 5 5 5 5 5 5 . . . . . 
-            . . . 5 5 5 5 5 5 5 5 5 . . . . 
-            . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-            . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-            . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 f f 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 f f 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            . . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            . . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-            . . . 5 5 5 5 5 5 5 5 5 5 5 . . 
-            . . . . 5 5 5 5 5 5 5 5 5 . . . 
-            . . . . . 5 5 5 5 5 5 5 . . . . 
-            `],
-        100,
-        true
-        )
-    }
-}
-function pacManMoveUp () {
-    if (controller.up.isPressed()) {
-        animation.runImageAnimation(
-        mySprite,
-        [img`
-            . . . . 5 5 . . . . 5 . . . . . 
-            . . . 5 5 5 . . . . 5 5 . . . . 
-            . . 5 5 5 5 . . . . 5 5 5 . . . 
-            . 5 5 5 5 5 . . . . 5 5 5 5 . . 
-            5 5 5 5 5 5 . . . . 5 5 5 5 . . 
-            5 5 5 5 5 5 . . . . 5 5 5 5 5 . 
-            5 5 5 5 5 5 5 . . 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 . . 5 5 5 5 5 5 5 
-            5 5 f f 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 f f 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-            . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-            . . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-            . . . . 5 5 5 5 5 5 5 5 5 . . . 
-            . . . . . 5 5 5 5 5 5 5 . . . . 
-            `,img`
-            . . . . 5 5 5 5 5 5 5 . . . . . 
-            . . . 5 5 5 5 5 5 5 5 5 . . . . 
-            . . 5 5 5 5 5 5 5 5 5 5 5 . . . 
-            . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 f f 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 f f 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-            . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-            . . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-            . . . . 5 5 5 5 5 5 5 5 5 . . . 
-            . . . . . 5 5 5 5 5 5 5 . . . . 
-            `],
-        100,
-        true
-        )
-    }
-}
-function pacManMoveRight () {
-    if (controller.right.isPressed()) {
-        animation.runImageAnimation(
-        mySprite,
-        [img`
-            . . . . 5 5 5 5 5 5 5 5 . . . . 
-            . . . 5 5 5 5 5 5 5 5 5 5 . . . 
-            . . 5 5 5 5 f f 5 5 5 5 5 5 . . 
-            . . 5 5 5 5 f f 5 5 5 5 5 5 5 . 
-            . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 . . . . . . 
-            5 5 5 5 5 5 5 5 . . . . . . . . 
-            5 5 5 5 5 5 5 5 . . . . . . . . 
-            5 5 5 5 5 5 5 5 5 5 . . . . . . 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-            . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-            . . 5 5 5 5 5 5 5 5 5 5 5 . . . 
-            . . 5 5 5 5 5 5 5 5 5 . . . . . 
-            . . . . . 5 5 5 5 5 . . . . . . 
-            `,img`
-            . . . . 5 5 5 5 5 5 5 5 . . . . 
-            . . . 5 5 5 5 5 5 5 5 5 5 . . . 
-            . . 5 5 5 5 f f 5 5 5 5 5 5 . . 
-            . . 5 5 5 5 f f 5 5 5 5 5 5 5 . 
-            . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-            . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-            . . 5 5 5 5 5 5 5 5 5 5 5 . . . 
-            . . 5 5 5 5 5 5 5 5 5 . . . . . 
-            . . . . . 5 5 5 5 5 . . . . . . 
-            `],
-        100,
-        true
-        )
-    }
-}
-scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestOpen, function (sprite, location) {
-    game.gameOver(true)
-    game.setGameOverEffect(true, effects.confetti)
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(assets.image`PacWoman`)
 })
+info.onCountdownEnd(function () {
+    if (myScore >= 75) {
+        game.gameOver(true)
+    } else {
+        game.gameOver(false)
+    }
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(img`
+        . . . . . 5 5 5 5 5 5 5 . . . . 
+        . . . 5 5 5 5 5 5 5 5 5 5 5 . . 
+        . . 5 5 5 5 5 5 f f 5 5 5 5 5 . 
+        . 5 5 5 5 5 5 5 f f 5 5 5 5 5 5 
+        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+        5 5 5 5 5 5 5 5 5 5 5 5 . . . . 
+        5 5 5 5 5 5 5 5 5 . . . . . . . 
+        5 5 5 5 5 5 5 . . . . . . . . . 
+        5 5 5 5 5 5 5 5 5 . . . . . . . 
+        5 5 5 5 5 5 5 5 5 5 5 5 . . . . 
+        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+        . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+        . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+        . . 5 5 5 5 5 5 5 5 5 5 5 . . . 
+        . . . 5 5 5 5 5 5 5 5 5 . . . . 
+        . . . . 5 5 5 5 5 5 . . . . . . 
+        `)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    game.gameOver(false)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    sprites.destroy(otherSprite, effects.confetti, 100)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleRedCrystal, function (sprite, location) {
+    if (myScore >= 50) {
+        game.gameOver(true)
+        game.showLongText(info.highScore(), DialogLayout.Bottom)
+    } else {
+        game.showLongText("Missing points!", DialogLayout.Bottom)
+        tiles.placeOnTile(mySprite, tiles.getTileLocation(21, 6))
+    }
+})
+let myEnemy: Sprite = null
+let pacFood: Sprite = null
 let mySprite: Sprite = null
+let myScore = 0
+info.setScore(0)
+myScore = 0
+let maxScore = 100
+scene.setBackgroundColor(15)
+tiles.setCurrentTilemap(tilemap`level1`)
 mySprite = sprites.create(img`
-    . . . . 5 5 5 5 5 5 5 . . . . . 
-    . . 5 5 5 5 5 5 5 5 5 5 5 . . . 
-    . 5 5 5 5 5 f f 5 5 5 5 5 5 . . 
-    5 5 5 5 5 5 f f 5 5 5 5 5 5 5 . 
-    . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-    . . . . 5 5 5 5 5 5 5 5 5 5 5 5 
-    . . . . . . . 5 5 5 5 5 5 5 5 5 
-    . . . . . . . . . 5 5 5 5 5 5 5 
-    . . . . . . . 5 5 5 5 5 5 5 5 5 
-    . . . . 5 5 5 5 5 5 5 5 5 5 5 5 
+    . . . . . 5 5 5 5 5 5 5 . . . . 
+    . . . 5 5 5 5 5 5 5 5 5 5 5 . . 
+    . . 5 5 5 5 5 5 f f 5 5 5 5 5 . 
+    . 5 5 5 5 5 5 5 f f 5 5 5 5 5 5 
+    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+    5 5 5 5 5 5 5 5 5 5 5 5 . . . . 
+    5 5 5 5 5 5 5 5 5 . . . . . . . 
+    5 5 5 5 5 5 5 . . . . . . . . . 
+    5 5 5 5 5 5 5 5 5 . . . . . . . 
+    5 5 5 5 5 5 5 5 5 5 5 5 . . . . 
     5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
     . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-    . . 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-    . . . 5 5 5 5 5 5 5 5 5 5 5 . . 
-    . . . . 5 5 5 5 5 5 5 5 5 . . . 
-    . . . . . . 5 5 5 5 5 5 . . . . 
+    . 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+    . . 5 5 5 5 5 5 5 5 5 5 5 . . . 
+    . . . 5 5 5 5 5 5 5 5 5 . . . . 
+    . . . . 5 5 5 5 5 5 . . . . . . 
     `, SpriteKind.Player)
-controller.moveSprite(mySprite)
-tiles.setCurrentTilemap(tilemap`level1`)
-tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 7))
+tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 8))
+controller.moveSprite(mySprite, 60, 60)
 scene.cameraFollowSprite(mySprite)
+info.startCountdown(120)
+for (let index = 0; index < maxScore; index++) {
+    pacFood = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . . . 3 1 1 3 . . . . . . 
+        . . . . . 2 1 1 1 1 2 . . . . . 
+        . . . . . 2 1 1 1 1 2 . . . . . 
+        . . . . . . 3 1 1 3 . . . . . . 
+        . . . . . . . 2 2 . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Food)
+    tiles.placeOnRandomTile(pacFood, assets.tile`transparency16`)
+}
+for (let index = 0; index < 4; index++) {
+    myEnemy = sprites.create(img`
+        ........................
+        ........................
+        ........................
+        ........................
+        ..........ffff..........
+        ........ff6666ff........
+        .......fb666666bf.......
+        .......f66666666f.......
+        ......fd66666666df......
+        ......fd66666666df......
+        ......fddd6666dddf......
+        ......fbdbfddfbdbf......
+        ......fcdcf66fcdcf......
+        .......fb666666bf.......
+        ......fffcdb6bdffff.....
+        ....fc166cbfbfc666cf....
+        ....f6b6b6ffff6b6b6f....
+        ....fbfbffffffbfbfbf....
+        .........ffffff.........
+        ...........fff..........
+        ........................
+        ........................
+        ........................
+        ........................
+        `, SpriteKind.Enemy)
+    tiles.placeOnRandomTile(myEnemy, assets.tile`transparency16`)
+    myEnemy.follow(mySprite, 45)
+}
 game.onUpdate(function () {
-    pacManMoveLeft()
-    pacManMoveRight()
-    pacManMoveDown()
-    pacManMoveUp()
+    if (info.score() > 0) {
+        myScore += 1
+    }
 })
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -2,28 +2,69 @@
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile15 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile17 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile18 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile19 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
+            case "level3":
+            case "level3":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
             case "level1":
-            case "level1":return tiles.createTilemap(hex`1e001000020202020202020202020202020202020202020202020202020202020202020101010101020101010101010101010101010101010101010101020102020102010201010102020202020202010202020202020201020201020102020102010201020102020202020202010201010101020201020101020102020102020201020101010101010101010201020102020201020102020102020101010101020102020202020102010201020102010101020101010102020202020201020102010101010102010201020102020202020102020202010101010101020102020202020102010201020101010101020101010103020202020201010101010102020102010201020102020201020102020202020101010201020202020202020102010101020101010201010101010102020102020201020202020202020102020202020102020201020202020102020102020201020202020101010101010101010102010101020201020102020101010101010101020102020202020201010102010101010101020102020102020202020201020202020101010201010202020102020202020102020101010101010101010101010101010101010101010101010101010102020202020202020202020202020202020202020202020202020202020202`, img`
-222222222222222222222222222222
-2.....2....................2.2
-2.2.2...2222222.2222222.22.2.2
-2.2.2.2.2222222.2....22.2..2.2
-2.222.2.........2.2.222.2.22.2
-2.....2.22222.2.2.2.2...2....2
-22222.2.2.....2.2.2.22222.2222
-......2.22222.2.2.2.....2.....
-22222......22.2.2.2.222.2.2222
-2...2.2222222.2...2...2......2
-2.222.2222222.22222.222.2222.2
-2.222.2222..........2...22.2.2
-2........2.222222...2......2.2
-2.222222.2222...2..222.22222.2
-2............................2
-222222222222222222222222222222
-`, [myTiles.transparency16,sprites.builtin.forestTiles10,sprites.builtin.forestTiles28,sprites.dungeon.chestOpen], TileScale.Sixteen);
+            case "level1":return tiles.createTilemap(hex`1700100007050505050505050505050505050505050505050505080600000000000000000000000000000000000000000006060007050505080001050505050505080007050508000606000600000006000000000000000006000600010900060600060003000600030007050502000600060000000006060004000a05090004000a0505050509000a050508000a060000000000000000000000000000000000000006000b0a050505050200010505050508000705050508000600070c0c0c0c0c0c0c0c0c0c0c0c0600040000000600060006070505050800070505050800060000000300060006000606000000060006000000060006000300060006000600060600030006000a05020006000400060006000600060006060006000600000000000600000006000600060006000606000a0509000105050509000300060004000a0509000606000000000000000000000006000600000000000000060a050505050505050505050509000a0505050505050509`, img`
+22222222222222222222222
+2.....................2
+2.22222.22222222.2222.2
+2.2...2........2.2.22.2
+2.2.2.2.2.2222.2.2....2
+2.2.222.2.222222.2222.2
+2...................2..
+222222.222222.22222.2.2
+............2.2...2.2.2
+22222.22222.2...2.2.2.2
+2...2.2...2.2.2.2.2.2.2
+2.2.2.222.2.2.2.2.2.2.2
+2.2.2.....2...2.2.2.2.2
+2.222.22222.2.2.2.222.2
+2...........2.2.......2
+2222222222222.222222222
+`, [myTiles.transparency16,myTiles.tile10,myTiles.tile11,myTiles.tile12,myTiles.tile13,myTiles.tile14,myTiles.tile15,myTiles.tile16,myTiles.tile17,myTiles.tile18,myTiles.tile19,sprites.dungeon.collectibleRedCrystal,sprites.builtin.forestTiles10], TileScale.Sixteen);
         }
         return null;
     })
@@ -31,6 +72,28 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
+            case "myTile7":
+            case "tile9":return tile9;
+            case "myTile8":
+            case "tile10":return tile10;
+            case "myTile9":
+            case "tile11":return tile11;
+            case "myTile10":
+            case "tile12":return tile12;
+            case "myTile11":
+            case "tile13":return tile13;
+            case "myTile12":
+            case "tile14":return tile14;
+            case "myTile13":
+            case "tile15":return tile15;
+            case "myTile14":
+            case "tile16":return tile16;
+            case "myTile15":
+            case "tile17":return tile17;
+            case "myTile16":
+            case "tile18":return tile18;
+            case "myTile17":
+            case "tile19":return tile19;
         }
         return null;
     })
